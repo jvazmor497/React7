24c7b928243186280a40e22ba2185e2b1e35bfc8
unknown <jvazmor497@g.educaand.es>
2025-01-19 04:24:54 +0100

    Añadido gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/React7/' : '',
   plugins: [react()],
 })
