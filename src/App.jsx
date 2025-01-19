a12b7402d70b5c0fb0454201afff550f8a297170
unknown <jvazmor497@g.educaand.es>
2025-01-19 04:19:54 +0100

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,204 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const champions = [
+  "Aatrox",
+  "Ahri",
+  "Akali",
+  "Alistar",
+  "Amumu",
+  "Anivia",
+  "Annie",
+  "Aphelios",
+  "Ashe",
+  "AurelionSol",
+  "Azir",
+  "Bard",
+  "Blitzcrank",
+  "Brand",
+  "Braum",
+  "Caitlyn",
+  "Camille",
+  "Cassiopeia",
+  "Chogath",
+  "Corki",
+  "Darius",
+  "Diana",
+  "Draven",
+  "DrMundo",
+  "Ekko",
+  "Elise",
+  "Evelynn",
+  "Ezreal",
+  "Fiddlesticks",
+  "Fiora",
+  "Fizz",
+  "Galio",
+  "Gangplank",
+  "Garen",
+  "Gnar",
+  "Gragas",
+  "Graves",
+  "Gwen",
+  "Hecarim",
+  "Heimerdinger",
+  "Illaoi",
+  "Irelia",
+  "Ivern",
+  "Janna",
+  "JarvanIV",
+  "Jax",
+  "Jayce",
+  "Jhin",
+  "Jinx",
+  "Kaisa",
+  "Kalista",
+  "Karma",
+  "Karthus",
+  "Kassadin",
+  "Katarina",
+  "Kayle",
+  "Kayn",
+  "Kennen",
+  "Khazix",
+  "Kindred",
+  "Kled",
+  "KogMaw",
+  "Leblanc",
+  "LeeSin",
+  "Leona",
+  "Lillia",
+  "Lissandra",
+  "Lucian",
+  "Lulu",
+  "Lux",
+  "Malphite",
+  "Malzahar",
+  "Maokai",
+  "MasterYi",
+  "MissFortune",
+  "Mordekaiser",
+  "Morgana",
+  "Nami",
+  "Nasus",
+  "Nautilus",
+  "Neeko",
+  "Nidalee",
+  "Nocturne",
+  "Nunu",
+  "Olaf",
+  "Orianna",
+  "Ornn",
+  "Pantheon",
+  "Poppy",
+  "Pyke",
+  "Qiyana",
+  "Quinn",
+  "Rakan",
+  "Rammus",
+  "RekSai",
+  "Rell",
+  "Renekton",
+  "Rengar",
+  "Riven",
+  "Rumble",
+  "Ryze",
+  "Samira",
+  "Sejuani",
+  "Senna",
+  "Seraphine",
+  "Sett",
+  "Shaco",
+  "Shen",
+  "Shyvana",
+  "Singed",
+  "Sion",
+  "Sivir",
+  "Skarner",
+  "Sona",
+  "Soraka",
+  "Swain",
+  "Sylas",
+  "Syndra",
+  "TahmKench",
+  "Taliyah",
+  "Talon",
+  "Taric",
+  "Teemo",
+  "Thresh",
+  "Tristana",
+  "Trundle",
+  "Tryndamere",
+  "TwistedFate",
+  "Twitch",
+  "Udyr",
+  "Urgot",
+  "Varus",
+  "Vayne",
+  "Veigar",
+  "Velkoz",
+  "Vex",
+  "Vi",
+  "Viego",
+  "Viktor",
+  "Vladimir",
+  "Volibear",
+  "Warwick",
+  "Wukong",
+  "Xayah",
+  "Xerath",
+  "XinZhao",
+  "Yasuo",
+  "Yone",
+  "Yorick",
+  "Yuumi",
+  "Zac",
+  "Zed",
+  "Ziggs",
+  "Zilean",
+  "Zoe",
+  "Zyra",
+];
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Random League of Legends Champion Generator</h1>
+      <ChampionCard />
+    </>
+  );
+}
+
+function ChampionCard() {
+
+  const [champion, setChampion] = useState(
+    champions[Math.floor(Math.random() * champions.length)]
+  );
+
+  // Cachea todas las imagenes en el navegador para que carguen más rapido cuando sea necesario
+  useEffect(() => {
+    champions.forEach((champ) => {
+      const img = new Image();
+      img.src = `https://ddragon.leagueoflegends.com/cdn/13.10.1/img/champion/${champ}.png`;
+    });
+  }, []);
+
+  const champ_image =
+    "https://ddragon.leagueoflegends.com/cdn/13.10.1/img/champion/" +
+    champion +
+    ".png";
+
+  return (
+    <div
+      className="card"
+      onClick={() =>
+        setChampion(champions[Math.floor(Math.random() * champions.length)])
+      }
+    >
+      <img src={champ_image} alt={champion} />
+      <h2>{champion}</h2>
+    </div>
+  );
+}
+
+export default App;
